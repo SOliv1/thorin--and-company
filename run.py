b13eec243182c3a8ffb2368dd1d12e00d13965e0
Sara Oliver <s.oliver1@icloud.com>
2020-01-20 16:47:12 +0000

    Update run.py

--- run.py
+++ run.py
@@ -41,6 +41,6 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 if __name__ == '__main__':
-    app.run (host=os.environ.get('IP'),
+    app.run(host=os.environ.get('IP'),
             port=int(os.environ.get("PORT")),
             debug=True)
