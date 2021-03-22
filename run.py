df4f7c227b41571660fc5a41b88e401dbe047876
Sara Oliver <s.oliver1@icloud.com>
2021-03-22 16:33:14 +0000

    home-pge image add

--- run.py
+++ run.py
@@ -45,5 +45,5 @@ def careers():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get("PORT")),
+            port=int(os.environ.get("PORT", "8000")),
             debug=True)
